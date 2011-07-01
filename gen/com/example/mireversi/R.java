50ea695222631637b80eeefa4a3a778639757cca
Makoto Ishida <makotoishida@gmail.com>
2011-06-30 20:59:49 -1000

    Deleted MainActivity

--- gen/com/example/mireversi/R.java
+++ gen/com/example/mireversi/R.java
@@ -14,12 +14,19 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int txtTop=0x7f050000;
+        public static final int mnuExit=0x7f060002;
+        public static final int mnuPref=0x7f060001;
+        public static final int txtTop=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class menu {
+        public static final int mainmenu=0x7f050000;
+    }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int mnu_exit=0x7f040001;
+        public static final int mnu_pref=0x7f040002;
     }
 }
--- src/com/example/mireversi/GameActivity.java
+++ src/com/example/mireversi/GameActivity.java
@@ -2,15 +2,47 @@ package com.example.mireversi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class GameActivity extends Activity{
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(new ReversiView(this));
-		
+
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.mainmenu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+//	@Override
+//	public boolean onPrepareOptionsMenu(Menu menu) {
+//
+//		return super.onPrepareOptionsMenu(menu);
+//	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()){
+		case R.id.mnuExit:
+			finish();
+			break;
+//		case R.id.mnuPref:
+//			openPref();
+//			break;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
