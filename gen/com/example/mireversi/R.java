88a53d94e10a099163797b430338fb9a3427a87f
Makoto Ishida <makotoishida@gmail.com>
2011-09-16 10:31:15 -1000

    Added images for background.

--- gen/com/example/mireversi/R.java
+++ gen/com/example/mireversi/R.java
@@ -50,8 +50,9 @@ public final class R {
     public static final class drawable {
         public static final int a6=0x7f020000;
         public static final int b1=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int w1=0x7f020003;
+        public static final int green=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int w1=0x7f020004;
     }
     public static final class id {
         public static final int frame=0x7f0b0000;
